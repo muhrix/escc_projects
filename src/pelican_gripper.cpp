1efe5e65b5eaf8543e7c10c8ea4919c27e7e60f8
Murilo FM <muhrix+pluto@gmail.com>
2014-11-24 16:46:46 -0200

    Fixed values sent over serial port

--- src/pelican_gripper.cpp
+++ src/pelican_gripper.cpp
@@ -16,6 +16,7 @@
 
 int fd;
 int ctrl_switch;
+unsigned char cmd;
 
 //for (int i = 0; i < cnt; i++) {
 //    write(fd, &tbyte[i], 1);
@@ -29,65 +30,65 @@ int ctrl_switch;
 // LEFT: 37
 
 void callback(const asctec_hlp_comm::mav_rcdataConstPtr& msg) {
-    unsigned char cmd;
     if (msg->channel.at(ctrl_switch) < 200) {
-        cmd = 0x28; //unsigned char(40);
-        write(fd, &cmd, 1);
+        cmd--;
         ROS_INFO("Closing gripper");
     }
     else if (msg->channel.at(ctrl_switch) > 4000) {
-        cmd = 0x27; //unsigned char(38);
-        write(fd, &cmd, 1);
+        cmd++;
         ROS_INFO("Opening gripper");
     }
+    write(fd, &cmd, 1);
 }
 
 int main(int argc, char* argv[]) {
-  ros::init(argc, argv, "gripper");
-  ros::NodeHandle n;
-  ros::NodeHandle n_priv("~");
-  std::string port;
-  int baud;
-  n_priv.param<std::string>("port", port, std::string("/dev/ttyACM0"));
-  n_priv.param<int>("baud", baud, int(115200));
-  n_priv.param<int>("gripper_switch", ctrl_switch, int(6));
+    ros::init(argc, argv, "gripper");
+    ros::NodeHandle n;
+    ros::NodeHandle n_priv("~");
+    std::string port;
+    int baud;
+    n_priv.param<std::string>("port", port, std::string("/dev/ttyACM0"));
+    n_priv.param<int>("baud", baud, int(115200));
+    n_priv.param<int>("gripper_switch", ctrl_switch, int(6));
 
-  fd = open(port.c_str(), O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
-  struct termios port_settings; // structure to store the port settings in
+    cmd = 48;
 
-  switch (baud) {
-  case 9600:
-      cfsetispeed(&port_settings, B9600);    // set baud rates
-      port_settings.c_cflag = B9600 | CS8 | CREAD | CLOCAL;
-      break;
-  case 57600:
-      cfsetispeed(&port_settings, B57600);    // set baud rates
-      port_settings.c_cflag = B57600 | CS8 | CREAD | CLOCAL;
-      break;
-  case 115200:
-      cfsetispeed(&port_settings, B115200);    // set baud rates
-      port_settings.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
-      break;
-  case 230400:
-      cfsetispeed(&port_settings, B230400);    // set baud rates
-      port_settings.c_cflag = B230400 | CS8 | CREAD | CLOCAL;
-      break;
-  default:
-      cfsetispeed(&port_settings, B115200);    // set baud rates
-      port_settings.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
-  }
-  //cfsetispeed(&port_settings, B230400);    // set baud rates
-  //port_settings.c_cflag = B230400 | CS8 | CREAD | CLOCAL;
-  port_settings.c_iflag = IGNPAR;
-  port_settings.c_oflag = 0;
-  port_settings.c_lflag = 0;
-  tcsetattr(fd, TCSANOW, &port_settings); // apply the settings to the port
+    fd = open(port.c_str(), O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
+    struct termios port_settings; // structure to store the port settings in
 
-  ros::Subscriber sub = n.subscribe<asctec_hlp_comm::mav_rcdata>("rcdata", 1, callback);
+    switch (baud) {
+    case 9600:
+        cfsetispeed(&port_settings, B9600);    // set baud rates
+        port_settings.c_cflag = B9600 | CS8 | CREAD | CLOCAL;
+        break;
+    case 57600:
+        cfsetispeed(&port_settings, B57600);    // set baud rates
+        port_settings.c_cflag = B57600 | CS8 | CREAD | CLOCAL;
+        break;
+    case 115200:
+        cfsetispeed(&port_settings, B115200);    // set baud rates
+        port_settings.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
+        break;
+    case 230400:
+        cfsetispeed(&port_settings, B230400);    // set baud rates
+        port_settings.c_cflag = B230400 | CS8 | CREAD | CLOCAL;
+        break;
+    default:
+        cfsetispeed(&port_settings, B115200);    // set baud rates
+        port_settings.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
+    }
+    //cfsetispeed(&port_settings, B230400);    // set baud rates
+    //port_settings.c_cflag = B230400 | CS8 | CREAD | CLOCAL;
+    port_settings.c_iflag = IGNPAR;
+    port_settings.c_oflag = 0;
+    port_settings.c_lflag = 0;
+    tcsetattr(fd, TCSANOW, &port_settings); // apply the settings to the port
 
-  ros::spin();
+    ros::Subscriber sub = n.subscribe<asctec_hlp_comm::mav_rcdata>("rcdata", 1, callback);
 
-  close(fd);
+    ros::spin();
 
-  return 0;
+    close(fd);
+
+    return 0;
 }
